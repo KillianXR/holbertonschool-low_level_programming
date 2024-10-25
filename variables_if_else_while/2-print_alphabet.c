53c726cefddc7ea8c965c5beb137e33c66f623ed
KillianXR <9756@holbertonstudents.com>
2024-10-25 09:30:10 +0000

    save

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*
+* char - function 
+*/
+
+int main(void)
+{	
+	char alphabet
+
+	for (alphabet = 'a'; alphabet <= 'z'; low++)
+		putchar(low);
+		putchar('\n');
+
+	return (0);
+}

dcc7fd94781349e8be84ff68f8f4254330ea5c90
KillianXR <9756@holbertonstudents.com>
2024-10-27 00:49:21 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,15 @@
-# include "main.h"
+#include "main.h"
 
 /**
  * print_alphabet_x10 - prints 10 times the alphabet
+ * followed by a new line
  */
 void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
+
+	i = 0;
 
 	while (i < 10)
 	{

cf34fbf301d3886c8b54814d1390dd2db591ea99
KillianXR <9756@holbertonstudents.com>
2024-10-29 10:13:23 +0000

    add 2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers
+ *
+ * @c: character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

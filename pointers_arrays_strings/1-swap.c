5c71dc5d9a5c030ef293bf41b6da8e5d159a448f
KillianXR <9756@holbertonstudents.com>
2024-11-02 13:07:45 +0100

    1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* swap_int - swaps the value of int a and int b
+*
+* @a: first intintager
+*
+* @b: second intager
+*
+* Return: Always 0 (Success)
+*/
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	a* = *b;
+	*b = tmp;
+}

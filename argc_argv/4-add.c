25b8848c65c16ff3c5cba07c6882bb94d485a2fe
KillianXR <9756@holbertonstudents.com>
2024-11-08 19:24:29 +0100

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiples two number
+ *
+ * @argc: count arguments
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0, res= 0;
+	char a[] = "Error", *fl;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			fl = argv[i];
+			while (*fl != 0)
+			{
+				if (*fl < 47 || *fl > 57)
+				{
+					printf("%s\n", a);
+					return (1);
+				}
+				fl++;
+			}
+			res = atoi(argv[i]);
+			sum += res;
+		}
+		printf("%d\n", sum);
+	}
+	else
+		printf("%d\n", 0);
+	return (0);
+}

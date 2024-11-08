f2b5a925cbffabb7f033bb2bf1f75069161855c1
KillianXR <9756@holbertonstudents.com>
2024-11-08 17:40:43 +0100

    betty

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,17 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main.c - entry point
+ * main - entry point
  *
  * @argc: number of arguments
  * @argv: name of arguments
+ *
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }

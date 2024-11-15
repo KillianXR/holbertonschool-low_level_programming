6fc62a9b7ff600988a9823797a06a6a1525822d8
KillianXR <9756@holbertonstudents.com>
2024-11-15 19:17:45 +0100

    fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,8 +1,54 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
 /**
  * new_dog - create a new dog
  * @name: name of the dog
@@ -21,22 +67,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 
-	name_cp = malloc(strlen(name) + 1);
+	name_cp = malloc(_strlen(name) + 1);
 	if (name_cp == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	strcpy(name_cp, name);
+	_strcpy(name_cp, name);
 
-	owner_cp = malloc(strlen(owner) + 1);
+	owner_cp = malloc(_strlen(owner) + 1);
 	if (owner_cp == NULL)
 	{
 		free(name_cp);
 		free(dog);
 		return (NULL);
 	}
-	strcpy(owner_cp, owner);
+	_strcpy(owner_cp, owner);
 
 	dog->name = name_cp;
 	dog->owner = owner_cp;
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -4,7 +4,7 @@
  * struct dog - dog description
  * @name: dog name
  * @age: dog age
- * @owner: dog owner 
+ * @owner: dog owner
  */
 struct dog
 {
@@ -26,5 +26,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif

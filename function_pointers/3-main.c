be4a36051310e1745c577d8f31d2d012196da396
KillianXR <9756@holbertonstudents.com>
2024-11-19 16:18:04 +0100

    3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -15,22 +15,22 @@ int main(int argc, char *argv[])
 {
 	char op;
 	int (*op_func)(int, int);
-	int result
+	int result;
 
 	if (argc != 4)
-	}
+	{
 		printf("Error\n");
 		return (98);
 	}
 	op = *(argv[2]);
-	if ((op_func == '/' || op == '%') && !atoi(argv[3]))
+	if ((op == '/' || op == '%') && !atoi(argv[3]))
 	{
 		printf("Error\n");
-		return(100);
+		return (100);
 	}
 
 	op_func = get_op_func(&op);
-	if (op_func == NULL || argv[2][1] != '/0')
+	if (op_func == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		return (99);

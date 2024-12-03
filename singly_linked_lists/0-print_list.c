8c47d99bbfa1f82b0dd7b32f24befef301c143bd
KillianXR <9756@holbertonstudents.com>
2024-12-03 12:59:10 +0100

    add 0-print_list functions to print the content of a list with a lists.h  who contain structure list, and functions. and a README.md to gives informations

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+	* print_list - print the content of a list
+	*
+	* @h: pointer to the list
+	*
+	* Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+		printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("[%u] (%s)\n", h->len, h->str);
+		}
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
